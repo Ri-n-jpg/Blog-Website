b2ef7f26fa1c551a74b465c2806e644351676e9d
Ritika Sharma <ritikasharma29112003@gmail.com>
2026-05-04 14:15:29 +0530

    updated code

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -73,13 +73,22 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 
 # 🗄 DATABASE
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import os
+import dj_database_url
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # 🔐 PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
